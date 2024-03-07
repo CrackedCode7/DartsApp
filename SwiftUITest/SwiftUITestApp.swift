bc488c35c41e8a8c690a8f9f20904704f9fd952a
Evan Rinehart <evanrinehart1@gmail.com>
2024-03-06 20:41:49 -0500

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Evan Rinehart on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
